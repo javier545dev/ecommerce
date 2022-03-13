2e8f9fd549401611fa9f7020b77e7034f2e5eece
javier545dev <javier545dev@github.com>
2022-03-12 22:36:02 -0500

    primer commit

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:prettier/recommended',
+    'standard'
+  ],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'no-unused-vars': 'off',
+    'no-useless-return': 'off',
+    'react/jsx-uses-vars': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'react/display-name': 'off',
+    'react/prop-types': 'off',
+    'prettier/prettier': ['off', {}, { usePrettierrc: true }] // Includes .prettierrc.js rules
+  }
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../styles/components/Header.css'
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <h1 className="Header-title">Ecommerce Pro</h1>
+      <div className="Header-checkout">
+        <button></button>
+        Checkout
+      </div>
+    </div>
+  )
+}
+
+export default Header
